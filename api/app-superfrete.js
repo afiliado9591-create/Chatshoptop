294541ab35f7917582a63ce816518c87959342fb
afiliado9591-create <afiliado9591@gmail.com>
2026-08-15 00:56:18 -0300

    Inject payment choice persistence fix on published stores

--- api/app-superfrete.js
+++ api/app-superfrete.js
@@ -88,6 +88,7 @@ module.exports = async function handler(request, response) {
     html = removeScript(html, 'store-affiliate-program.js');
     html = removeScript(html, 'mercadopago-oauth-ui.js');
     html = removeScript(html, 'virtual-payment-fix.js');
+    html = removeScript(html, 'payment-choice-persist.js');
 
     const virtualTag = '<script src="/virtual-shipping-upgrade.js?v=20260813-1707"></script>';
     const superfreteDomainFixTag = '<script src="/superfrete-domain-fix.js?v=20260814-1058"></script>';
@@ -103,7 +104,8 @@ module.exports = async function handler(request, response) {
     const planAccessTag = '<script src="/plan-access-control.js?v=20260814-1615"></script>';
     const storeAffiliateProgramTag = '<script src="/store-affiliate-program.js?v=20260814-1640"></script>';
     const mercadoPagoOauthTag = '<script src="/mercadopago-oauth-ui.js?v=20260814-1825"></script>';
-    const virtualPaymentFixTag = '<script src="/virtual-payment-fix.js?v=20260815-0035"></script>';
+    const paymentChoicePersistTag = '<script src="/payment-choice-persist.js?v=20260815-0105"></script>';
+    const virtualPaymentFixTag = '<script src="/virtual-payment-fix.js?v=20260815-0105"></script>';
     let inject = '';
     if (!html.includes('/virtual-shipping-upgrade.js')) inject += virtualTag + '\n';
     inject += superfreteDomainFixTag + '\n';
@@ -119,6 +121,7 @@ module.exports = async function handler(request, response) {
     inject += planAccessTag + '\n';
     inject += storeAffiliateProgramTag + '\n';
     inject += mercadoPagoOauthTag + '\n';
+    inject += paymentChoicePersistTag + '\n';
     inject += virtualPaymentFixTag + '\n';
     if (inject) {
       const pos = html.toLowerCase().lastIndexOf('</body>');
