0d59d1df2a1e289a9fe568fc0cdb935eacff121d
afiliado9591-create <afiliado9591@gmail.com>
2026-08-17 16:18:20 -0300

    Repete abertura do produto único (#70)
    * fix: retry product open until modern storefront is ready
    
    * chore: refresh product open retry

--- api/render.js
+++ api/render.js
@@ -201,7 +201,7 @@ function injectUpgrades(html, storefrontMode, layout) {
     html = forceScript(html, 'seller-audio-upload-fix.js', version);
   }
   html = forceScript(html, 'product-seller-button-control.js', version);
-  html = forceScript(html, 'virtual-single-product-mode.js', '20260817-2035-delay-open');
+  html = forceScript(html, 'virtual-single-product-mode.js', '20260817-2055-retry-open');
   return html;
 }
 
