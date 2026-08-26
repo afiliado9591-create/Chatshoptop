4185e46a90c92e64ece82029bdb4261d11d42d13
afiliado9591-create <afiliado9591@gmail.com>
2026-08-25 21:01:23 -0300

    Preservar escape HTML ao corrigir domínio próprio

--- api/render.js
+++ api/render.js
@@ -7,6 +7,7 @@ function htmlEscape(value) {
   return String(value ?? '')
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;')
     .replace(/'/g, '&#39;');
 }
